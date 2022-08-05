c7f75f792533c12353c45e6df2156ba6d1bbae5d
Edward Li <radioactivehydra@gmail.com>
2022-08-05 19:56:28 -0400

    Change how streaming logic is set up

--- wormhole/streamer/rawstreamer.py
+++ wormhole/streamer/rawstreamer.py
@@ -1,9 +1,4 @@
 from wormhole.streamer.socketiostreamer import SocketIOStreamerBase
-from wormhole.utils import FrameController
-
-from flask_socketio import emit
-import time
-from typing import Optional
 
 # Raw Image Streaming
 # This is generally not recommended as it sends A LOT of information over the network, 
@@ -13,27 +8,11 @@ class RawStreamer(SocketIOStreamerBase):
     def __init__(
         self, 
         *args,
-        fps_override: Optional[int] = None, 
         **kwargs
     ):
         # Initiate Parent SocketIO Streamer Object
-        super().__init__(self.video_streamer, *args, **kwargs)
-        
-        # Setup Basic Vars
-        self.max_fps = fps_override or self.video.max_fps
+        super().__init__(self.stream_hotloop, *args, **kwargs)
     
-    # Streamer hotloop for raw video
-    def video_streamer(self):
-        # Get the flask context
-        with self.controller.get_app().app_context():
-            frame_controller = FrameController(self.max_fps)
-            while True:
-                if not self.thread_running:
-                    return  # Kill the thread if no clients are connected
-                
-                emit("frame", self.video.get_frame().tobytes(), room="video_feed", namespace=self.route, broadcast=True)
-                
-                time.sleep(0)  # Force A Network Buffer Flush
-                # Technically this should be a gevent.sleep(0) but time.sleep is monkey patched so this should be alright.
-                
-                frame_controller.next_frame()
+    # Hotloop for sending raw video
+    def stream_hotloop(self):
+        self.send_data(self.video.get_frame().tobytes())
--- wormhole/streamer/socketiostreamer.py
+++ wormhole/streamer/socketiostreamer.py
@@ -1,20 +1,28 @@
 from wormhole.streamer import AbstractStreamer
+from wormhole.utils import FrameController
 
 from flask_socketio import join_room
 from threading import Thread
-from typing import Callable
+from typing import Callable, Optional, Any
+from flask_socketio import emit
+import time
 
 # Base Class for Everything SocketIO Streamer
 class SocketIOStreamerBase(AbstractStreamer):
     def __init__(
         self, 
-        video_streamer: Callable,
+        stream_publisher: Callable,
         *args, 
+        fps_override: Optional[int] = None, 
         **kwargs
     ):
         super().__init__(*args, **kwargs)
-        # Main hotloop to run for video streams
-        self.video_streamer = video_streamer
+        
+        # Setup Basic Vars
+        self.max_fps = fps_override or self.video.max_fps
+        
+        # Main loop to run for video streams
+        self.stream_publisher = stream_publisher
         
         # Control variables to save on execution when no clients are connected
         self.thread_running = False
@@ -44,3 +52,27 @@ class SocketIOStreamerBase(AbstractStreamer):
             if self.connected_clients == 0:
                 self.thread_running = False
         self.controller.add_message_handler("disconnect", on_disconnect, namespace=self.route, strict_url=self.strict_url)
+            
+    # Streamer simple  for raw video
+    def video_streamer(self):
+        # Get the flask context
+        with self.controller.get_app().app_context():
+            frame_controller = FrameController(self.max_fps)
+            while True:
+                if not self.thread_running:
+                    return  # Kill the thread if no clients are connected
+                
+                # Run Stream Publisher Function
+                # This should process any video encoding work and publishing logic
+                self.stream_publisher()
+                
+                frame_controller.next_frame()
+                
+    # Helper function to emit socket information
+    def send_data(self, data: Any):
+        emit("frame", data, room="video_feed", namespace=self.route, broadcast=True)
+        
+        time.sleep(0)  # Force A Network Buffer Flush
+        # Technically this should be a gevent.sleep(0) but time.sleep is monkey patched so this should be alright.
+        
+        return True
